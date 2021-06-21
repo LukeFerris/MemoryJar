3b51f7cbf89215b5a3674dbe522fd08051e4bfda
Luke Ferris <luke.ferris@gmail.com>
2021-06-21 19:19:32 +0200

    Added request upload url ldambda

--- api/responsibleFor/audio/requestUploadUrl/index.js
+++ api/responsibleFor/audio/requestUploadUrl/index.js
@@ -1,6 +1,33 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const uuid = require('uuidv4')
+const AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.REGION || 'eu-central-1' })
+const s3 = new AWS.S3();
 
-  return {};
-};
+exports.handler = async (event) => {
+  return await getUploadURL()
+}
+
+const getUploadURL = async () => {
+  const actionId = uuid()
+  const s3Params = {
+    Bucket: 'memoryjarbucket',
+    Key:  `${actionId}.webm`,
+    ContentType: 'audio/webm',
+    ACL: 'public-read',
+  }
+
+  return new Promise((resolve, reject) => {
+    let uploadURL = s3.getSignedUrl('putObject', s3Params)
+    resolve({
+      "statusCode": 200,
+      "isBase64Encoded": false,
+      "headers": { "Access-Control-Allow-Origin": "*" },
+      "body": JSON.stringify({
+        "uploadURL": uploadURL,
+        "photoFilename": `${actionId}.webm`,
+        "id": actionId
+      })
+    })
+  })
+}
+  
