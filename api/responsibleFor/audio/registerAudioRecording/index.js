d41fd0768077ee39092d4492a8b498e5ee22629a
Luke Ferris <luke.ferris@gmail.com>
2021-06-24 20:19:17 +0200

    Added first version register audio clip api

--- api/responsibleFor/audio/registerAudioRecording/index.js
+++ api/responsibleFor/audio/registerAudioRecording/index.js
@@ -1,6 +1,48 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+// require data-api
+const data = require('data-api-client')({
+  secretArn: process.env.DB_ROOT_USER_SECRET_ARN,
+  resourceArn: process.env.DB_ARN,
+  database: 'memoryjar'
+})
 
-  return {};
-};
+exports.handler = async (event, context) => {
+
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "GET /audio_clips":
+        body = await data.query(`SELECT * from audio_clip;`);
+        break;
+      case "PUT /audio_clips":
+        let clip = JSON.parse(event.body);
+
+        await data.query(
+          `INSERT INTO audio_clip (memory_id, audio_clip_id) VALUES(:memory_id,:audio_clip_id)`,
+          clip
+        )
+
+        body = `Put item ${clip.audio_clip_id}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+
+}
+
