32bd7bf3787817d03da9fd3b81a1c9895af0eaf1
Luke Ferris <luke.ferris@gmail.com>
2021-06-24 19:42:08 +0200

    Changed copyright to memory jar

--- site/src/Components/AudioList.js
+++ site/src/Components/AudioList.js
@@ -22,7 +22,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="">
-        forefront.studio
+        MemoryJar
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
