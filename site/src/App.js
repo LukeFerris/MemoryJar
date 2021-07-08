fb7685a6d75b8d529d6532ffe05f9e2a0eaf632c
Luke Ferris <luke.ferris@gmail.com>
2021-07-08 16:14:38 +0200

    Basic sign in page and routing if not logged in

--- site/src/App.js
+++ site/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import AudioList from './Components/AudioList'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import SignIn from './Components/SignIn'
+
+let isLoggedIn = false;
 
 function App() {
   return (
-    <div className="App">
-      <AudioList />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => (
+            isLoggedIn ? (
+              <AudioList />
+            ) : (
+              <Redirect to="/signin" />
+            )
+          )} />
+          <Route exact path='/signin' component={SignIn} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
