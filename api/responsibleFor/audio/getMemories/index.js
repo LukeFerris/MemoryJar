1590a6319958c89f3567f0134fdb72c4f6583707
Luke Ferris <luke.ferris@gmail.com>
2021-07-17 15:11:19 +0200

    Broke out get memories into separate lambda

--- api/responsibleFor/audio/getMemories/index.js
+++ api/responsibleFor/audio/getMemories/index.js
@@ -1,6 +1,43 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+// require data-api
+const data = require('data-api-client')({
+  secretArn: process.env.DB_ROOT_USER_SECRET_ARN,
+  resourceArn: process.env.DB_ARN,
+  database: 'memoryjar'
+})
 
-  return {};
-};
+exports.handler = async (event, context) => {
+
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    // pull public key from database cache
+    let keys = await data.query('SELECT * from keys');
+
+    if (keys.records.length == 0) {
+      console.log('no keys present in database, downloading..');
+      break;
+    }
+    // if it doesn't exist, pull it from Amazon and cache it
+    // validate incoming jwt
+    // extract user id
+    // return audio clips only for selected user
+    body = await data.query(`SELECT * from audio_clip;`);
+    break;
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+
+}
--- api/responsibleFor/audio/registerAudioRecording/index.js
+++ api/responsibleFor/audio/registerAudioRecording/index.js
@@ -14,37 +14,29 @@ exports.handler = async (event, context) => {
   };
 
   try {
-    switch (event.routeKey) {
-      case "GET /":
-        body = await data.query(`SELECT * from audio_clip;`);
-        break;
-      case "PUT /":
-        let clip = JSON.parse(event.body);
+    let clip = JSON.parse(event.body);
 
-        // check to see if the memory exists
-        let memory = await data.query("SELECT COUNT(*) from memory WHERE memory_id = '" + clip.memory_id + "';");
-        let memoryCount = memory.records[0].count;
-        console.log('found ' + memoryCount + ' memories with that id');
+    // check to see if the memory exists
+    let memory = await data.query("SELECT COUNT(*) from memory WHERE memory_id = '" + clip.memory_id + "';");
+    let memoryCount = memory.records[0].count;
+    console.log('found ' + memoryCount + ' memories with that id');
 
-        if (memoryCount == 0) {
-          // insert the memory first
-          await data.query(
-            `INSERT INTO memory (memory_id) VALUES(:memory_id::UUID)`,
-            clip
-          )
-        }
-
-        // now insert the audio clip
-        await data.query(
-          `INSERT INTO audio_clip (memory_id, audio_clip_id) VALUES(:memory_id::UUID,:audio_clip_id::UUID)`,
-          clip
-        )
-
-        body = `Put item ${clip.audio_clip_id}`;
-        break;
-      default:
-        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    if (memoryCount == 0) {
+      // insert the memory first
+      await data.query(
+        `INSERT INTO memory (memory_id) VALUES(:memory_id::UUID)`,
+        clip
+      )
     }
+
+    // now insert the audio clip
+    await data.query(
+      `INSERT INTO audio_clip (memory_id, audio_clip_id) VALUES(:memory_id::UUID,:audio_clip_id::UUID)`,
+      clip
+    )
+
+    body = `Put item ${clip.audio_clip_id}`;
+    break;
   } catch (err) {
     statusCode = 400;
     body = err.message;
