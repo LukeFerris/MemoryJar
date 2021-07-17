ed3771f9c26f992b1f9e50fdcc37562305ac00c9
Luke Ferris <luke.ferris@gmail.com>
2021-07-17 15:28:39 +0200

    Added current keys

---
+++ db/scripts/17072021a-insertKeys.sql
@@ -0,0 +1,5 @@
+DELETE FROM keys;
+
+INSERT INTO keys (key_id, data, created_on) VALUES ('7PU0WHq24PLOcsHWiw4qHl1izrTmPEeuRrdwVqHZwb8=', '{"alg":"RS256","e":"AQAB","kid":"7PU0WHq24PLOcsHWiw4qHl1izrTmPEeuRrdwVqHZwb8=","kty":"RSA","n":"pqm-z1lOfi_mkKolJFBGu_EsrQF1Vx35Lm9GXJ3d_XXL5NB3OYT78Rt-A5JJBZfSUTdX77CzasxJi8zeqgkjYbglJ5ZlKHCjv48k6cPz8NIQjuYzyOPf1ev7ZXuQMcfkhXqrbSb01BrosUzl_2bEY25E_RnsUxPL6JK0Bl1VMlRSyBsJbqb_FwPv0bOFeoyzc89nJOQ1v044RaE42bVpUNVdwuyPBUftpdacjgRMASaBWa4a0NtlwQ36laMSJogH8c7BhFYvoMX0pqRaOCyIKyTsGS8bqScdoqKIMOG02oJPcyufJzLHsU1JS_JrU3R4bLjHI3kz6Q3L8Venz9OBtQ","use":"sig"}', current_timestamp);
+INSERT INTO keys (key_id, data, created_on) VALUES ('xciyH61Pki9q85d770jIonYUc9GTj97QhbqJpbuHmp8=', '{"alg":"RS256","e":"AQAB","kid":"xciyH61Pki9q85d770jIonYUc9GTj97QhbqJpbuHmp8=","kty":"RSA","n":"68uVhPdmNWFOcm3IlSNFlcanVfCuVq4767WEnFsgzgr3wyT1pU8oY0LiUjb7ZtSjsdff7ySGEpL4AB7dYxf4L0Mv9Va11e-LkEcvwH6G89EKRBV-yT3n1ahMtNJA5da1Bcg4JhXQEpYoM7-S9H9hU_sIzKhjiezZtsj_EviA28x2tztjIE59fpHnCte96DNVeXABwnmNUR4tPx9zku3ohTNWQqn8fAdw9JY9LzJHvu_-2OlQbQNquZOUaYpycCkoMuRc5DLzRzmrRmnFxywavZ6r2lgAfx4gpcrz1-rxx4zQggXzf2b5arPOhVDZtkdvD9W_YiimNPMCBE-p2BnKyw","use":"sig"}', current_timestamp);
+
