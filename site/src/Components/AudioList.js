56901ec09fa12f48dc8625bfc9888b8d1f7fcd37
Luke Ferris <luke.ferris@gmail.com>
2021-07-19 13:56:19 +0200

    Tidy up of AudioList component

--- site/src/Components/AudioList.js
+++ site/src/Components/AudioList.js
@@ -1,60 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/styles';
-import Link from '@material-ui/core/Link';
 import axios from 'axios';
 import AudioItem from '../Components/AudioItem';
 import Recorder from '../Components/Recorder';
 import useToken from '../Components/useToken';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="">
-        MemoryJar
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  loadingProgress: {
-    marginBottom: theme.spacing(1)
-  },
-  appBar: {
-    backgroundColor: '#000'
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  }
-}));
-
 export default function AudioList() {
-  const classes = useStyles();
-  const [loading, setLoading] = useState(false);
+
   const [data, setData] = useState([]);
   const [uploadedFiles, setUploadedFiles] = useState([]);
 
@@ -72,7 +24,7 @@ export default function AudioList() {
         }
       );
 
-      if (result.data) {
+      if (result.data.records.length > 0) {
         setData(result.data.records.sort((a, b) => (a.created_time_stamp < b.created_time_stamp) ? 1 : -1));
       }
       else {
@@ -107,11 +59,14 @@ export default function AudioList() {
       <Grid item xs={12} sm={6} md={12}>
         <Recorder onFileUploaded={complete} />
       </Grid>
-      {data.map((audioClip) => (
+      {data.length > 0 ? data.map((audioClip) => (
         <Grid item key={audioClip.audio_clip_id} xs={12} sm={6} md={4}>
           <AudioItem audioClipId={audioClip.audio_clip_id} />
         </Grid>
-      ))}
+      )) :
+        <Grid item xs={12} sm={6} md={12}>
+          <p>A lot of nothing here.. add a clip!</p>
+        </Grid>}
     </Grid>
   );
 }
