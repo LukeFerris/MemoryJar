5d0609eb52f820756c0182849c106cf315c6a2b1
Luke Ferris <luke.ferris@gmail.com>
2021-07-20 11:58:36 +0200

    Added basic getThemes API call

--- api/responsibleFor/audio/getThemes/index.js
+++ api/responsibleFor/audio/getThemes/index.js
@@ -1,6 +1,31 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+// require data-api
+const data = require('data-api-client')({
+  secretArn: process.env.DB_ROOT_USER_SECRET_ARN,
+  resourceArn: process.env.DB_ARN,
+  database: 'memoryjar'
+})
 
-  return {};
-};
+exports.handler = async (event, context) => {
+
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    body = await data.query("select * from theme join prompt on theme.theme_id = prompt.theme_id;");
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+
+}
