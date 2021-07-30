837f527e6bbdfa579b93f3805ba0fb60db6c0cc7
Luke Ferris <luke.ferris@gmail.com>
2021-07-30 13:21:42 +0200

    Basic delete media item function

--- api/responsibleFor/mediaItems/deleteMediaItem/index.js
+++ api/responsibleFor/mediaItems/deleteMediaItem/index.js
@@ -1,6 +1,51 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+// require data-api
+const data = require('data-api-client')({
+  secretArn: process.env.DB_ROOT_USER_SECRET_ARN,
+  resourceArn: process.env.DB_ARN,
+  database: 'memoryjar'
+})
 
-  return {};
-};
+exports.handler = async (event, context) => {
+
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+
+    if (typeof event.requestContext == 'undefined') {
+      console.log('No valid user supplied');
+      body = "No valid user supplied";
+      statusCode = 400;
+    }
+    else {
+      let mediaItem = JSON.parse(event.body);
+
+      // force user on clip to be the authorised user
+      mediaItem.userId = event.requestContext.authorizer.jwt.claims.sub;
+      console.log('user is: ' + mediaItem.userId);
+
+      // find the prompt associated with this media item
+      let deleteResult = await data.query("DELETE FROM \"mediaItem\" WHERE \"mediaItemId\" = '" + mediaItem.mediaItemId + "'");
+
+      body = `Deleted media item ${mediaItem.mediaItemId}`;
+    }
+  }
+  catch (err) {
+    statusCode = 400;
+    console.log(err.message);
+    body = err.message;
+  }
+  finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers
+  };
+
+}
---
+++ db/migrations/20210728a-add-deleted-column.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "mediaItem"
+ADD COLUMN "deleted" Boolean;
+
+ALTER TABLE "memory"
+ADD COLUMN "deleted" Boolean;
