077394350c70eaa97afca3bf506b8efdb5d1d90b
Luke Ferris <luke.ferris@gmail.com>
2021-08-14 13:13:46 +0200

    Added basic lambda call to the processing lambda

--- api/responsibleFor/mediaItems/uploadMediaItem/index.js
+++ api/responsibleFor/mediaItems/uploadMediaItem/index.js
@@ -1,3 +1,7 @@
+var AWS = require('aws-sdk');
+AWS.config.region = 'eu-west-1';
+var lambda = new AWS.Lambda();
+
 // require data-api
 const data = require('data-api-client')({
   secretArn: process.env.DB_ROOT_USER_SECRET_ARN,
@@ -39,82 +43,91 @@ exports.handler = Sentry.AWSLambda.wrapHandler(async (event, context) => {
       console.log('user is: ' + mediaItem.userId);
 
       // await process and upload files to S3 using separate lambda
-      // await add items to databse
-        // check if the item already exists
-      let mediaItemResult = await data.query("SELECT * from \"mediaItem\" WHERE \"mediaItemId\" = '" + mediaItem.mediaItemId + "'");
+      var params = {
+        FunctionName: process.env.FUNCTION_NAME, // the lambda function we are going to invoke
+        InvocationType: 'RequestResponse',
+        LogType: 'Tail',
+        Payload: '{"image" : "XXX", "mediaItemId": "YYY" }'
+      };
+    
+      lambda.invoke(params, function(err, data) {
 
-      if (mediaItemResult.records.length == 0) {
+          // now that it's successfully uploaded, check if the item already exists
+        let mediaItemResult = await data.query("SELECT * from \"mediaItem\" WHERE \"mediaItemId\" = '" + mediaItem.mediaItemId + "'");
 
-        // item does not exist so register it
-        // find the prompt associated with this media item
-        let promptResult = await data.query("SELECT * from prompt WHERE \"promptId\" = '" + mediaItem.promptId + "'");
+        if (mediaItemResult.records.length == 0) {
 
-        if (promptResult.records.length == 0) {
-          body = "No prompts with id: " + mediaItem.promptId + " could be found";
-          statusCode = 400;
-        }
-        else {
-          console.log('found prompt with id: ' + mediaItem.promptId);
-          const prompt = promptResult.records[0];
+          // item does not exist so register it
+          // find the prompt associated with this media item
+          let promptResult = await data.query("SELECT * from prompt WHERE \"promptId\" = '" + mediaItem.promptId + "'");
 
-          // see if a memory for this theme already exists for this user
-          let memoryResult = await data.query("SELECT * from memory WHERE \"themeId\" = '" + prompt.themeId + "' AND \"userId\"='" + mediaItem.userId + "';");
-
-          let memory;
-
-          // if not, create it
-          if (memoryResult.records.length == 0) {
-
-            console.log('No memory found for this theme and user, creating..');
-            memory = {
-              memoryId: context.awsRequestId,
-              themeId: prompt.themeId,
-              userId: mediaItem.userId
-            }
-
-            // insert the memory first
-            await data.query(
-              'INSERT INTO memory ("memoryId", "themeId", "userId") VALUES(:memoryId::UUID, :themeId::UUID, :userId::UUID)',
-              memory
-            )
+          if (promptResult.records.length == 0) {
+            body = "No prompts with id: " + mediaItem.promptId + " could be found";
+            statusCode = 400;
           }
           else {
-            console.log(memoryResult.records[0]);
-            console.log('found memory for theme: ' + prompt.themeId + ' for user. Id is: ' + memoryResult.records[0].memoryId);
-            memory = memoryResult.records[0];
-          }
+            console.log('found prompt with id: ' + mediaItem.promptId);
+            const prompt = promptResult.records[0];
 
-          // now insert the audio clip
-          const newMediaItem =
-          {
-            mediaItemId: mediaItem.mediaItemId,
-            promptId: prompt.promptId,
-            memoryId: memory.memoryId,
-            userId: mediaItem.userId,
-            mediaType: mediaItem.mediaType,
-            relatedMediaItemId: mediaItem.relatedMediaItemId
-          }
+            // see if a memory for this theme already exists for this user
+            let memoryResult = await data.query("SELECT * from memory WHERE \"themeId\" = '" + prompt.themeId + "' AND \"userId\"='" + mediaItem.userId + "';");
 
-          await data.query(
-            'INSERT INTO "mediaItem" ("memoryId", "promptId", "mediaItemId", "userId", "mediaType", "relatedMediaItemId") VALUES(:memoryId::UUID, :promptId::UUID, :mediaItemId::UUID, :userId::UUID, :mediaType::SMALLINT, :relatedMediaItemId::UUID)',
-            newMediaItem
-          )
+            let memory;
 
-          // if a related media item was present - update the partner mediaitem
-          if (newMediaItem.relatedMediaItemId) {
+            // if not, create it
+            if (memoryResult.records.length == 0) {
+
+              console.log('No memory found for this theme and user, creating..');
+              memory = {
+                memoryId: context.awsRequestId,
+                themeId: prompt.themeId,
+                userId: mediaItem.userId
+              }
+
+              // insert the memory first
+              await data.query(
+                'INSERT INTO memory ("memoryId", "themeId", "userId") VALUES(:memoryId::UUID, :themeId::UUID, :userId::UUID)',
+                memory
+              )
+            }
+            else {
+              console.log(memoryResult.records[0]);
+              console.log('found memory for theme: ' + prompt.themeId + ' for user. Id is: ' + memoryResult.records[0].memoryId);
+              memory = memoryResult.records[0];
+            }
+
+            // now insert the audio clip
+            const newMediaItem =
+            {
+              mediaItemId: mediaItem.mediaItemId,
+              promptId: prompt.promptId,
+              memoryId: memory.memoryId,
+              userId: mediaItem.userId,
+              mediaType: mediaItem.mediaType,
+              relatedMediaItemId: mediaItem.relatedMediaItemId
+            }
+
             await data.query(
-              `UPDATE "mediaItem" SET "relatedMediaItemId"=\'${newMediaItem.mediaItemId}\' WHERE "mediaItemId"=\'${newMediaItem.relatedMediaItemId}\';`
+              'INSERT INTO "mediaItem" ("memoryId", "promptId", "mediaItemId", "userId", "mediaType", "relatedMediaItemId") VALUES(:memoryId::UUID, :promptId::UUID, :mediaItemId::UUID, :userId::UUID, :mediaType::SMALLINT, :relatedMediaItemId::UUID)',
+              newMediaItem
             )
+
+            // if a related media item was present - update the partner mediaitem
+            if (newMediaItem.relatedMediaItemId) {
+              await data.query(
+                `UPDATE "mediaItem" SET "relatedMediaItemId"=\'${newMediaItem.mediaItemId}\' WHERE "mediaItemId"=\'${newMediaItem.relatedMediaItemId}\';`
+              )
+            }
           }
+
+          body = `Post media item ${mediaItem.mediaItemId}`; 
         }
-        
-        body = `Post media item ${mediaItem.mediaItemId}`; 
-      }
-      else
-      {
-        body = "MediaItem with id: " + mediaItem.mediaItemId + " already exists.";
-        statusCode = 400;
-      }
+        else
+        {
+          body = "MediaItem with id: " + mediaItem.mediaItemId + " already exists.";
+          statusCode = 400;
+        }
+      })
     }
   }
   catch (err) {
